5c0ec854e572d64f6e1e949ab4fb822aed8b52b2
victorviss <victorviss@gmail.com>
2024-04-09 21:21:53 -0300

    feat: Inclusão componentes header e forms

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { FormsComponent } from './components/forms/forms.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, FormsComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  providers: [HeaderComponent, FormsComponent]
+})
+export class AppComponent {
+  title = 'smartfit-frontend';
+}
